2b58268d1d3ed18c49f4bdef673abc17fa8ef5b8
FVOCI <150913557+fvoci@users.noreply.github.com>
2025-04-22 02:55:26 +0900

    Update preload_all.py

--- app/preload/preload_all.py
+++ app/preload/preload_all.py
@@ -1,16 +1,20 @@
 import whisper
+import gc
 
-def load_whisper_models():
+def preload_whisper_models():
     model_names = ['base', 'large', 'medium', 'small', 'tiny', 'turbo']
-    models = {}
     for name in model_names:
         try:
             print(f"Loading model: {name}...")
-            models[name] = whisper.load_model(name)
-            print(f"Model '{name}' loaded successfully.\n")
+            model = whisper.load_model(name)
+            print(f"Model '{name}' loaded successfully.")
+            
+            del model
+            gc.collect()
+            print(f"Model '{name}' unloaded.\n")
+
         except Exception as e:
             print(f"Error loading model '{name}': {e}\n")
-    return models
 
 if __name__ == "__main__":
-    loaded_models = load_whisper_models()
+    preload_whisper_models()
